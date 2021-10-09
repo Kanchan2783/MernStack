443bb5b6a3ef2f98016b7a2dc84daa56a3683603
Kanchan2783 <kanchan.shirbhaiyye1996@gmail.com>
2021-10-09 09:04:18 +0530

    my first commit

---
+++ kanchandemo/src/Apps.js
@@ -0,0 +1,13 @@
+import code from './vendor/images/Code.jpg';
+export default function App(){
+
+   
+return(
+    <>
+    <p> My Name Is Kanchan</p>
+    <hr/>
+    <input/>
+    <img className="myImg" src={ code} />
+    </>
+)
+}
---
+++ kanchandemo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import './vendor/css/style.css';
+import App from "./Apps";
+
+
+ReactDOM.render(<App />, document.getElementById('root')) ;
